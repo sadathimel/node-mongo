79b8c7160530eff46155b1c7d7d6135c16019a14
sadat <sadat.himel@gmail.com>
2020-10-16 01:23:18 +0600

    dynamic api

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const express = require('express');
 
 const app = express();
 
+const users = ["Asad", "Moin", "Sabed", "Susmita", "Sohana"];
+
 
 app.get('/', (req,res)=>{
-    res.send('i know how to open Node...yay!')
+    const fruit ={
+        product: 'ada',
+        price:220
+    }
+    res.send(fruit)
+});
+app.get('/fruits/banana', (req,res)=>{
+    res.send({fruit: 'banana', quantity: 1000, price:200})
+})
+app.get('/users/:id', (req,res) =>{
+    const id= req.params.id;
+    console.log(req.query.sort);
+    const name = users[id];
+    res.send({id, name});
 })
 
 app.listen(4200,()=> console.log('Listening to port 4200'))
